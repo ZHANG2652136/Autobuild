dba485458e42cf97699d866bbf49fcb50d73d2fd
ZHANG2652136 <104855410+ZHANG2652136@users.noreply.github.com>
2024-01-01 20:25:35 +0800

    Update 01_customize_packages.sh

--- scripts/01_customize_packages.sh
+++ scripts/01_customize_packages.sh
@@ -4,51 +4,51 @@
 [[ ! -d package/new ]] && mkdir -p package/new
 
 # Access Control
-cp -rf ../immortalwrt-luci/applications/luci-app-accesscontrol package/new/
+#cp -rf ../immortalwrt-luci/applications/luci-app-accesscontrol package/new/
 
 # arpbind
-cp -rf ../immortalwrt-luci/applications/luci-app-arpbind package/new/
+#cp -rf ../immortalwrt-luci/applications/luci-app-arpbind package/new/
 
 # AutoCore
-cp -rf ../immortalwrt/package/emortal/autocore package/new/
-cp -rf ../immortalwrt/package/utils/mhz package/utils/
-rm -rf feeds/luci/modules/luci-base
-cp -rf ../immortalwrt-luci/modules/luci-base feeds/luci/modules
-rm -rf feeds/luci/modules/luci-mod-status
-cp -rf ../immortalwrt-luci/modules/luci-mod-status feeds/luci/modules/
+#cp -rf ../immortalwrt/package/emortal/autocore package/new/
+#cp -rf ../immortalwrt/package/utils/mhz package/utils/
+#rm -rf feeds/luci/modules/luci-base
+#cp -rf ../immortalwrt-luci/modules/luci-base feeds/luci/modules
+#rm -rf feeds/luci/modules/luci-mod-status
+#cp -rf ../immortalwrt-luci/modules/luci-mod-status feeds/luci/modules/
 
 # automount
-cp -rf ../lede/package/lean/automount package/new/
-cp -rf ../lede/package/lean/ntfs3-mount package/new/
+#cp -rf ../lede/package/lean/automount package/new/
+#cp -rf ../lede/package/lean/ntfs3-mount package/new/
 
 # cpufreq
-cp -rf ../immortalwrt-luci/applications/luci-app-cpufreq package/new/
+#cp -rf ../immortalwrt-luci/applications/luci-app-cpufreq package/new/
 
 # dnsmasq
-rm -rf package/network/services/dnsmasq
-cp -rf ../immortalwrt/package/network/services/dnsmasq package/network/services/
+#rm -rf package/network/services/dnsmasq
+#cp -rf ../immortalwrt/package/network/services/dnsmasq package/network/services/
 
 # mbedtls
-rm -rf ./package/libs/mbedtls
-cp -rf ../immortalwrt/package/libs/mbedtls package/libs/
+#rm -rf ./package/libs/mbedtls
+#cp -rf ../immortalwrt/package/libs/mbedtls package/libs/
 
 # OLED
-svn export -q https://github.com/NateLol/luci-app-oled/trunk package/new/luci-app-oled
+#svn export -q https://github.com/NateLol/luci-app-oled/trunk package/new/luci-app-oled
 
 # openwrt-packages
-svn export -q https://github.com/haiibo/openwrt-packages package/new/openwrt-packages
+#svn export -q https://github.com/haiibo/openwrt-packages package/new/openwrt-packages
 
 # Realtek RTL8125/8125B/8126A
-svn export -q https://github.com/sbwml/package_kernel_r8125/trunk package/new/r8125
+#svn export -q https://github.com/sbwml/package_kernel_r8125/trunk package/new/r8125
 
 # Realtek RTL8152/8153
-cp -rf ../immortalwrt/package/kernel/r8152 package/new/
+#cp -rf ../immortalwrt/package/kernel/r8152 package/new/
 
 # Release Ram
-cp -rf ../immortalwrt-luci/applications/luci-app-ramfree package/new/
+#cp -rf ../immortalwrt-luci/applications/luci-app-ramfree package/new/
 
 # Scheduled Reboot
-cp -rf ../immortalwrt-luci/applications/luci-app-autoreboot package/new/
+#cp -rf ../immortalwrt-luci/applications/luci-app-autoreboot package/new/
 
 # ShadowsocksR Plus+
 #svn export -q https://github.com/fw876/helloworld/trunk package/helloworld
@@ -56,11 +56,11 @@ cp -rf ../immortalwrt-luci/applications/luci-app-autoreboot package/new/
 #rm -rf ./feeds/packages/net/{xray-core,shadowsocks-libev}
 
 # USB Printer
-cp -rf ../immortalwrt-luci/applications/luci-app-usb-printer package/new/
+#cp -rf ../immortalwrt-luci/applications/luci-app-usb-printer package/new/
 
 # vlmcsd
-cp -rf ../immortalwrt-luci/applications/luci-app-vlmcsd package/new/
-cp -rf ../immortalwrt-packages/net/vlmcsd package/new/
+#cp -rf ../immortalwrt-luci/applications/luci-app-vlmcsd package/new/
+#cp -rf ../immortalwrt-packages/net/vlmcsd package/new/
 
 # Zerotier
 #cp -rf ../immortalwrt-luci/applications/luci-app-zerotier package/new/
